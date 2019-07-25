5acbf4e9fc5cba95a9118b8b25cbf68da562f974
Ebere Uzodufa <ebereuzodufa@gmail.com>
2019-07-25 10:10:35 +0100

    Creates my Octopus (ViemModel/Controller)
    This helps with communication between my model and my intended view.
    
    createStudent helps me create an object of student that contains the student name, array of attendance and, number of attendance. It uses the number of attendance days (in the model) to generate false and input it in the attendance array.
    
    addStudent function helps create and add  student to the model studentList array.
    
    getNumOfAttendDays function returns the number of attendance in the model.
    
    setNumOfAttendDays function - this set the number of attendance in the model.
    
    getSelectedStudent function - this returns selected student.
    
    setSelectedStudent - this sets student from the fed varible.
    
    getStudentList - gets the studentList from the model
    
    updateSelectedStudentAttendance - updates selected student atendance record.
    
    updateSelectedName - updates selected student name

--- js/app.js
+++ js/app.js
@@ -3,3 +3,43 @@ var model = {
     numOfAttendanceDays: 7,
     selectedStudent: null
 };
+
+var octopus = {
+    createStudent: function(name, numAttenDays){
+        let student = new Object({
+            name: name,
+            attendance:[],
+            numAttended: 0
+        });
+        for (let i = 0; i < numAttenDays; i++) {
+            student.attendance.push(false);
+        }
+        return student;
+    },
+    addStudent: function(name){
+        const numAttenDays = octopus.getNumOfAttendDays();
+        const student = octopus.createStudent(name, numAttenDays);
+        model.studentList.push(student);
+    },
+    getNumOfAttendDays: function(){
+        return model.numOfAttendanceDays;
+    },
+    setNumOfAttendDays: function(noOfDays) {
+        model.numOfAttendanceDays = noOfDays;
+    },
+    getSelectedStudent: function(){
+        return model.selectedStudent;
+    },
+    setSelectedStudent: function(theStudent) {
+        model.selectedStudent = theStudent;
+    },
+    getStudentList: function(){
+        return model.studentList;
+    },
+    updateSelectedStudentAttendance: function(thisStudentAttendance) {
+        model.selectedStudent.attendance = thisStudentAttendance;
+    },
+    updateSelectedName: function(thisStudentName) {
+        model.selectedStudent.name = thisStudentName;
+    }
+}
