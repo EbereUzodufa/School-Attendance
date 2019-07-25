f2dcbca49b6c9bad6304a23d1c7fef8b6d56d4ff
Ebere Uzodufa <ebereuzodufa@gmail.com>
2019-07-25 10:43:27 +0100

    Changes student model and adds comments

--- js/app.js
+++ js/app.js
@@ -8,17 +8,18 @@ var octopus = {
     createStudent: function(name, numAttenDays){
         let student = new Object({
             name: name,
-            attendance:[],
-            numAttended: 0
+            attendance:[]
+            //numAttended: 0 - I can get this data from countin the number of false/true
         });
+        //Since the attendance is an array of bool, we make it zero by populating the field with zero
         for (let i = 0; i < numAttenDays; i++) {
             student.attendance.push(false);
         }
         return student;
     },
     addStudent: function(name){
-        const numAttenDays = octopus.getNumOfAttendDays();
-        const student = octopus.createStudent(name, numAttenDays);
+        const numAttenDays = octopus.getNumOfAttendDays(); //Get the number of attendance from the model
+        const student = octopus.createStudent(name, numAttenDays);// ^ and use it as a varible for this function with name
         model.studentList.push(student);
     },
     getNumOfAttendDays: function(){
@@ -37,9 +38,11 @@ var octopus = {
         return model.studentList;
     },
     updateSelectedStudentAttendance: function(thisStudentAttendance) {
+        //Since the selected object is set to a student in the studentList array, I can update it here and it will also take effects on the studentList array.
         model.selectedStudent.attendance = thisStudentAttendance;
     },
     updateSelectedName: function(thisStudentName) {
+        //Since the selected object is set to a student in the studentList array, I can update it here and it will also take effects on the studentList array.
         model.selectedStudent.name = thisStudentName;
     }
 }
